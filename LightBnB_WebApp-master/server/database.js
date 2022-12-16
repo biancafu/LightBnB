43aee5e9f92e786fb550fbe91d94656ba77e645f
Bianca Fu <0509biancafu@gmail.com>
2022-12-16 01:02:35 +0000

    edited getAllReservations so that it is getting all the data it needs to show correct information on reservation page

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -66,7 +66,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.*, properties.title, properties.cost_per_night, properties.thumbnail_photo_url, properties.cover_photo_url, avg(rating) as average_rating
+  FROM properties
+  JOIN reservations ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(result => {
+    console.log(result);
+    return result.rows;
+  })
+  .catch(err => console.log(err));
 }
 exports.getAllReservations = getAllReservations;
 
